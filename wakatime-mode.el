3a2f1226db44556cb70c7ae88907701308e801fc
Gábor Török <gabor.torok@prezi.com>
2013-12-21 12:16:12 +0100

    Add docstrings and comments

--- wakatime-mode.el
+++ wakatime-mode.el
@@ -1,3 +1,7 @@
+;;; package --- Minor mode for WakaTime
+;;; Commentary:
+;;; Code:
+
 (defconst wakatime-user-agent "wakatime-mode")
 
 (defgroup wakatime nil
@@ -6,16 +10,17 @@
   :prefix "wakatime-")
 
 (defcustom wakatime-api-key nil
-  "API key for WakaTime"
+  "API key for WakaTime."
   :type 'string
   :group 'wakatime)
 
 (defcustom wakatime-client-path nil
-  "Path of CLI client for WakaTime"
+  "Path of CLI client for WakaTime."
   :type 'string
   :group 'wakatime)
 
 (defun wakatime-client-command (savep)
+  "Return client command.  Set SAVEP to non-nil for write action."
   (format "python %s --file %s %s --plugin %s --key %s"
           wakatime-client-path
           (buffer-file-name (current-buffer))
@@ -24,19 +29,23 @@
           wakatime-api-key))
 
 (defun wakatime-ping ()
+  "Send ping notice to WakaTime."
   (shell-command
    (wakatime-client-command nil)))
 
 (defun wakatime-save ()
+  "Send save notice to WakaTime."
   (shell-command
    (wakatime-client-command t)))
 
 (defun wakatime-turn-on ()
+  "Turn on WakaTime."
   (add-hook 'after-save-hook 'wakatime-save nil t)
   (add-hook 'auto-save-hook 'wakatime-save nil t)
   (add-hook 'first-change-hook 'wakatime-ping nil t))
 
 (defun wakatime-turn-off ()
+  "Turn off WakaTime."
   (remove-hook 'after-save-hook 'wakatime-save t)
   (remove-hook 'auto-save-hook 'wakatime-save t)
   (remove-hook 'first-change-hook 'wakatime-ping t))
@@ -57,3 +66,4 @@
     (wakatime-turn-off))))
 
 (provide 'wakatime-mode)
+;;; wakatime-mode.el ends here
