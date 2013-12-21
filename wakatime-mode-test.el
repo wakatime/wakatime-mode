60867710dcb6d91f58251f67c4a8b296f40132e2
Gábor Török <gabor.torok@prezi.com>
2013-12-21 15:32:28 +0100

    Add unit test suite

---
+++ wakatime-mode-test.el
@@ -0,0 +1,12 @@
+(require 'ert)
+(require 'wakatime-mode)
+
+(ert-deftest wakatime-client-command ()
+    ""
+  (let ((wakatime-api-key "secret")
+        (wakatime-cli-path "client")
+        (wakatime-user-agent "wakatime-mode"))
+    (should
+     (equal (wakatime-client-command t) "/usr/bin/python client --file nil --write --plugin wakatime-mode --key secret"))
+    (should
+     (equal (wakatime-client-command nil) "/usr/bin/python client --file nil  --plugin wakatime-mode --key secret"))))
