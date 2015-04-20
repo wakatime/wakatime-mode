2b7e2c4697e0814c7e3af65d7e3c41f1b3c132e1
Alan Hamlett <alan.hamlett@gmail.com>
2015-04-20 00:46:46 -0700

    prevent infinite recursion when prompting for api key or cli path. don't warn about response code 102, because that just means we are working offline.

--- wakatime-mode.el
+++ wakatime-mode.el
@@ -1,10 +1,11 @@
-;;; wakatime-mode.el --- Automatic time tracking extension based on WakaTime
+;;; wakatime-mode.el --- Automatic time tracking extension for WakaTime
 
 ;; Copyright (C) 2013  Gabor Torok <gabor@20y.hu>
 
 ;; Author: Gabor Torok <gabor@20y.hu>
+;; Website: https://wakatime.com
 ;; Keywords: calendar, comm
-;; Version: 0.1
+;; Version: 1.0.0
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
@@ -29,73 +30,112 @@
 ;; `wakatime-cli-path' to the absolute path of the CLI script
 ;; (wakatime-cli.py).
 
-;;; Code:
-
+(defconst wakatime-version "1.0.0")
 (defconst wakatime-user-agent "emacs-wakatime")
-
-(defconst wakatime-error-codes '((api . 102)))
+(setq noprompt nil)
 
 (defgroup wakatime nil
   "Customizations for WakaTime"
   :group 'convenience
-  :prefix "wakatime-")
+  :prefix "wakatime-"
+)
 
 (defcustom wakatime-api-key nil
   "API key for WakaTime."
   :type 'string
-  :group 'wakatime)
+  :group 'wakatime
+)
 
 (defcustom wakatime-cli-path nil
   "Path of CLI client for WakaTime."
   :type 'string
-  :group 'wakatime)
+  :group 'wakatime
+)
 
 (defcustom wakatime-python-bin "python"
   "Path of Python binary."
   :type 'string
-  :group 'wakatime)
+  :group 'wakatime
+)
+
+(defun wakatime-init ()
+  (if (or (not wakatime-api-key) (string= "" wakatime-api-key))
+    (wakatime-prompt-api-key)
+  )
+  (if (or (not wakatime-cli-path) (not (file-exists-p wakatime-cli-path)))
+    (wakatime-prompt-cli-path)
+  )
+)
+
+(defun wakatime-prompt-api-key ()
+  "Prompt user for api key."
+  (when (and (= (recursion-depth) 0) (not noprompt))
+    (setq noprompt t)
+    (let ((api-key (read-string "WakaTime API key: ")))
+      (customize-set-variable 'wakatime-api-key api-key)
+      (customize-save-customized)
+    )
+    (setq noprompt nil)
+  )
+)
+
+(defun wakatime-prompt-cli-path ()
+  "Prompt user for cli path."
+  (when (and (= (recursion-depth) 0) (not noprompt))
+    (setq noprompt t)
+    (let ((cli-path (read-file-name "WakaTime CLI script path: ")))
+      (customize-set-variable 'wakatime-cli-path cli-path)
+      (customize-save-customized)
+    )
+    (setq noprompt nil)
+  )
+)
 
 (defun wakatime-client-command (savep)
   "Return client command executable and arguments.
-Set SAVEP to non-nil for write action."
-  (format "%s %s --file %s %s --plugin %s --key %s --time %.2f"
-          wakatime-python-bin
-          wakatime-cli-path
-          (buffer-file-name (current-buffer))
-          (if savep "--write" "")
-          wakatime-user-agent
-          wakatime-api-key
-          (float-time)))
-
-(defun wakatime-init ()
-  (if (or (not wakatime-api-key)
-          (string= "" wakatime-api-key))
-      (let ((api-key (read-string "WakaTime API key: ")))
-        (customize-save-variable 'wakatime-api-key api-key)))
-  (if (or (not wakatime-cli-path)
-          (not (file-exists-p wakatime-cli-path)))
-      (let ((cli-path (read-string "WakaTime CLI script path: ")))
-        (customize-save-variable 'wakatime-cli-path cli-path))))
+   Set SAVEP to non-nil for write action."
+  (format "%s %s --file %s %s --plugin %s/%s --key %s --time %.2f"
+    wakatime-python-bin
+    wakatime-cli-path
+    (buffer-file-name (current-buffer))
+    (if savep "--write" "")
+    wakatime-user-agent
+    wakatime-version
+    wakatime-api-key
+    (float-time)
+  )
+)
 
 (defun wakatime-call (command)
   "Call WakaTime COMMAND."
-  (let ((process (start-process "Shell" (generate-new-buffer " *WakaTime messages*")
-                                shell-file-name shell-command-switch command)) )
-    (set-process-sentinel
-     process
-     (lambda (process signal)
-       (when (memq (process-status process) '(exit signal))
-         (let ((exit-status (process-exit-status process)))
-           (cond
-            ((= (cdr (assoc 'api wakatime-error-codes)) exit-status)
-             (progn
-               (error "An error occured while connecting to WakaTime (code %s)." exit-status)))
-            ((< 0 exit-status)
-             (progn
-               (error "Unexpected WakaTime error occured (code %s)!"
-                      exit-status))))
-           (kill-buffer (process-buffer process))))))
-    (set-process-query-on-exit-flag process nil)))
+  (let
+    (
+      (process
+        (start-process
+          "Shell"
+          (generate-new-buffer " *WakaTime messages*")
+          shell-file-name
+          shell-command-switch
+          command
+        )
+      )
+    )
+
+    (set-process-sentinel process
+      (lambda (process signal)
+        (when (memq (process-status process) '(exit signal))
+          (let ((exit-status (process-exit-status process)))
+            (if (and (not (= 0 exit-status)) (not (= 102 exit-status)))
+              (progn (error "WakaTime Error (%s)" exit-status))
+            )
+          )
+        )
+      )
+    )
+
+    (set-process-query-on-exit-flag process nil)
+  )
+)
 
 (defun wakatime-ping ()
   "Send ping notice to WakaTime."
@@ -112,35 +152,33 @@ Set SAVEP to non-nil for write action."
   (wakatime-init)
   (add-hook 'after-save-hook 'wakatime-save nil t)
   (add-hook 'auto-save-hook 'wakatime-save nil t)
-  (add-hook 'first-change-hook 'wakatime-ping nil t))
+  (add-hook 'first-change-hook 'wakatime-ping nil t)
+)
 
 (defun wakatime-turn-off ()
   "Turn off WakaTime."
   (remove-hook 'after-save-hook 'wakatime-save t)
   (remove-hook 'auto-save-hook 'wakatime-save t)
-  (remove-hook 'first-change-hook 'wakatime-ping t))
+  (remove-hook 'first-change-hook 'wakatime-ping t)
+)
 
 ;;;###autoload
 (define-minor-mode wakatime-mode
-  "Toggle WakaTime (WakaTime mode).
-With a prefix argument ARG, enable Whitespace mode if ARG is
-positive, and disable it otherwise.  If called from Lisp, enable
-the mode if ARG is omitted or nil."
+  "Toggle WakaTime (WakaTime mode)."
   :lighter    " waka"
   :init-value nil
   :global     nil
   :group      'wakatime
   (cond
-   (noninteractive
-    (setq wakatime-mode nil))
-   (wakatime-mode
-    (wakatime-turn-on))
-   (t
-    (wakatime-turn-off))))
+    (noninteractive (setq wakatime-mode nil))
+    (wakatime-mode (wakatime-turn-on))
+    (t (wakatime-turn-off))
+  )
+)
 
 ;;;###autoload
 (define-globalized-minor-mode global-wakatime-mode wakatime-mode (lambda () (wakatime-mode 1)))
-
+  
+(message "Initializing WakaTime v%s" wakatime-version)
 
 (provide 'wakatime-mode)
-;;; wakatime-mode.el ends here
